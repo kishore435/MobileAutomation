90ec8c68b13c951b22eabe3e850006cfbd0b722e
kishore435 <kishorekumarR105@gmail.com>
2024-10-14 14:49:47 +0530

    first commit

---
+++ Appium/src/test/java/KishoreProjects/AppiumBasics.java
@@ -0,0 +1,34 @@
+package KishoreProjects;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+public class AppiumBasics {
+	
+	@Test
+	public void AppiumTest() throws MalformedURLException, URISyntaxException
+	{
+		//To start Appium Server
+		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\Mobile Programming\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("KishoreEmulator");
+		options.setApp("C:\\Users\\Mobile Programming\\MobileAutomation\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+		
+		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+		driver.quit();
+		service.stop();
+		
+	}
+
+}

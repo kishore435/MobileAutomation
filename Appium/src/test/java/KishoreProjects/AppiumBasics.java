bc544d7dd7537764b6d2d12557c59c1a762e6161
kishore435 <kishorekumarR105@gmail.com>
2024-10-18 01:20:04 +0530

    Aded code for first test case

--- Appium/src/test/java/KishoreProjects/AppiumBasics.java
+++ Appium/src/test/java/KishoreProjects/AppiumBasics.java
@@ -1,5 +1,7 @@
 package KishoreProjects;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -20,7 +22,13 @@ public class AppiumBasics extends BaseTest {
 	{
 		
 		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-		
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+		driver.findElement(By.id("android:id/checkbox")).click();
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+		Assert.assertEquals(alertTitle,"WiFi settings");
+		driver.findElement(By.id("android:id/edit")).sendKeys("Kishore WI-FI");
+		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
 	}
 
 }
--- Appium/src/test/java/KishoreProjects/BaseTest.java
+++ Appium/src/test/java/KishoreProjects/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -30,6 +31,7 @@ public class BaseTest {
 				options.setApp("C:\\Users\\Mobile Programming\\MobileAutomation\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
 				
 				 driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+				 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
 	@AfterClass

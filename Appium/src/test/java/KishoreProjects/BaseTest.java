e9186f15bc4b46b9add67ae99c5b3cf64a333977
kishore435 <kishorekumarR105@gmail.com>
2024-10-19 00:35:57 +0530

    added long press program

--- Appium/src/test/java/KishoreProjects/BaseTest.java
+++ Appium/src/test/java/KishoreProjects/BaseTest.java
@@ -6,8 +6,14 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -32,6 +38,12 @@ public class BaseTest {
 				
 				 driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
 				 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	public void longPress(WebElement ele)
+	{
+		((JavascriptExecutor)driver).executeScript("mobile: longClickGesture", ImmutableMap.of("elementId", ((RemoteWebElement) ele).getId(), "duration",2000));
+		
 	}
 	
 	@AfterClass
